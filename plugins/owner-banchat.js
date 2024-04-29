b9a50fcaad33fec7ccdb1d274e95e564818d2321
jihadtc <94998329+jihadtc@users.noreply.github.com>
2024-04-29 15:03:32 +0100

    Delete plugins directory

--- plugins/owner-banchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = true
-m.reply('*ثم حظر هاذا المستخدم* ✓')
-}
-handler.help = ['banchat']
-handler.tags = ['owner']
-handler.command = /^banchat|bangp$/i
-handler.rowner = true
-export default handler
